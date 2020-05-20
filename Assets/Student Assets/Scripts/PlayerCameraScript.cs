61f79006574651589ed91c95b6f33acdc5cd4054
CrazyCyfro <crazycyfro@gmail.com>
2020-05-20 18:28:22 +0800

    Exercise 2: Vertical Rotation

---
+++ Assets/Student Assets/Scripts/PlayerCameraScript.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCameraScript : MonoBehaviour
+{
+    public Camera playerCamera;
+    private float camVertAngle = 0;
+    void Update()
+    {
+        UpdateCameraRotation();
+    }
+    float LookInputX()
+    {
+        return Input.GetAxisRaw("Mouse X");
+    }
+
+    float LookInputY()
+    {
+        return Input.GetAxisRaw("Mouse Y");
+    }
+
+    void UpdateCameraRotation()
+    {
+        // Horizontal rotation
+        transform.Rotate(Vector3.up, LookInputX());
+
+        // Vertical rotation
+        camVertAngle += LookInputY();
+        camVertAngle = Mathf.Clamp(camVertAngle, -89f, 89f);
+        playerCamera.transform.localEulerAngles = new Vector3(-camVertAngle, 0 , 0);
+    }
+}
