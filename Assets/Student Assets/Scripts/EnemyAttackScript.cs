ba941ebc783d95cbe10e9e4df73b3acfb7d244ae
CrazyCyfro <crazycyfro@gmail.com>
2020-06-10 18:25:07 +0800

    Exercise 6: Attack Logic (part 2)

--- Assets/Student Assets/Scripts/EnemyAttackScript.cs
+++ Assets/Student Assets/Scripts/EnemyAttackScript.cs
@@ -4,6 +4,22 @@ using UnityEngine;
 
 public class EnemyAttackScript : MonoBehaviour
 {
+    private IEnumerator attack;
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.tag == "Player") {
+            attack = Attack(collider.gameObject.GetComponent<HealthBase>(), 2.0f);
+            StartCoroutine(attack);
+        }
+    }
+
+    void OnTriggerExit(Collider collider)
+    {
+        if (collider.gameObject.tag == "Player") {
+            StopCoroutine(attack);
+        }
+    }
     IEnumerator Attack(HealthBase target, float interval)
     {
         while (!target.Dead()) {
