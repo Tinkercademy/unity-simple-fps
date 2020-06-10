0ea7ffa93abc300d9f8a73678e2b0883b96ac82c
CrazyCyfro <crazycyfro@gmail.com>
2020-06-10 18:19:43 +0800

    Exercise 4: Player Collision (part 2)

--- Assets/Student Assets/Scripts/EnemyNavScript.cs
+++ Assets/Student Assets/Scripts/EnemyNavScript.cs
@@ -12,4 +12,23 @@ public class EnemyNavScript : MonoBehaviour
     {
         agent.SetDestination(playerData.playerPos);
     }
+
+    void OnTriggerEnter(Collider collider)
+    {
+        if (collider.gameObject.tag == "Player") {
+            agent.isStopped = true;
+            agent.velocity = Vector3.zero;
+        }
+    }
+
+    // Restart NavMeshAgent after separating from player
+
+    void OnTriggerExit(Collider collider)
+    {
+        if (collider.gameObject.tag == "Player") {
+            agent.isStopped = false;
+        }
+    }
 }
+
+
