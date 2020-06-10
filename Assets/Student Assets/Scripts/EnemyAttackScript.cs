b608563e0299c4786a85c42cb8245dbc8af97349
CrazyCyfro <crazycyfro@gmail.com>
2020-06-10 18:22:16 +0800

    Exercise 5: Attack (part 2)

---
+++ Assets/Student Assets/Scripts/EnemyAttackScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAttackScript : MonoBehaviour
+{
+    IEnumerator Attack(HealthBase target, float interval)
+    {
+        while (!target.Dead()) {
+            target.TakeDamage(1);
+
+            yield return new WaitForSeconds(interval);
+        }
+
+    }
+}
