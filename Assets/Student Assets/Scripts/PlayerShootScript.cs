37270b53f26aee9e0d24c84d2fbf6ce25df18ec2
CrazyCyfro <crazycyfro@gmail.com>
2020-06-10 17:06:21 +0800

    Exercise 5: Impact Check

---
+++ Assets/Student Assets/Scripts/PlayerShootScript.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShootScript : MonoBehaviour
+{
+    public Camera playerCamera;
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1")) {
+            Fire();
+        }
+    }
+
+    void Fire()
+    {
+        // Check for impact. If absent, return.
+        RaycastHit hit;
+        if (!Physics.Raycast(playerCamera.transform.position, playerCamera.transform.forward, out hit, Mathf.Infinity)) return;
+
+        // Check if Zombie is hit
+        if (hit.collider.gameObject.tag == "Zombie") {
+            HealthBase zombie = hit.collider.gameObject.GetComponent<HealthBase>();
+            zombie.TakeDamage(1);
+        }
+    }
+}
