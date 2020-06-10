8271f31bfa0c0d01e740cca127b15a287672f726
CrazyCyfro <crazycyfro@gmail.com>
2020-06-10 17:01:13 +0800

    Exercise 4: Health

---
+++ Assets/Student Assets/Scripts/EnemyHealthScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealthScript : HealthBase
+{
+    public int initHealth;
+
+    void Start()
+    {
+        health = initHealth;
+    }
+}
