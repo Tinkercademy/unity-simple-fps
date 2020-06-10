e384bc3bd59a8aed161d78906d244f47f10d7a78
CrazyCyfro <crazycyfro@gmail.com>
2020-06-10 17:56:27 +0800

    Exercise 3: Player Health (part 2)

---
+++ Assets/Student Assets/Scripts/PlayerHealthScript.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerHealthScript : HealthBase
+{
+    public int initHealth;
+    
+    void Start()
+    {
+        health = initHealth;
+    }
+
+    public override void Die()
+    {
+        Debug.Log("I seem to be dead");
+    }
+}
